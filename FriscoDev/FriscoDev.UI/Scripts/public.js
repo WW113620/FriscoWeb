00d84a4cdbe42072eda1f9c8ed7c5d2fe44f7489
Hert <Hert@DESKTOP-K4IKU47>
2020-03-31 10:33:01 +0800

    2

--- FriscoDev/FriscoDev.UI/Scripts/public.js
+++ FriscoDev/FriscoDev.UI/Scripts/public.js
@@ -64,6 +64,23 @@ function StrTrim(str) {
     return str.replace(/(^\s*)|(\s*$)/g, "");
 }
 
+
+function is_mobile() {
+    var regex_match = /(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i;
+    var u = navigator.userAgent;
+    if (null == u) {
+        return true;
+    }
+    var result = regex_match.exec(u);
+    if (null == result) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+
+
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
